86bbff51d5a77e681ab9a12cb5fad08854e8baa0
Vadim <darksteellord@gmail.com>
2020-10-07 21:39:45 +0300

    routing improved

---
+++ src/route_utils.py
@@ -0,0 +1,49 @@
+from typing import Awaitable, Callable
+
+import json
+
+from aiohttp import web
+
+
+ALLOWED_NAME_SYMBOLS = [chr(i) for i in range(ord('а'), ord('я') + 1)] +\
+    [chr(i) for i in range(ord('А'), ord('Я') + 1)] +\
+    [chr(i) for i in range(ord('0'), ord('9') + 1)] + [' ']
+
+
+def handle_incorrect_name(
+        func: Callable[[web.Request], Awaitable[web.Response]]
+    ) -> Callable[[web.Request], Awaitable[web.Response]]:
+    async def handler(request: web.Request) -> web.Response:
+        json_from_request = await request.json()
+
+        if not "name" in json_from_request:
+            return await func(request)
+        elif all(map(
+                lambda symbol: symbol in ALLOWED_NAME_SYMBOLS,
+                json_from_request["name"]
+        )):
+            return await func(request)
+        else:
+            raise ValueError("Incorrect symbol in 'name'")
+
+    return handler
+
+def handle_error(
+        func: Callable[[web.Request], Awaitable[web.Response]]
+    ) -> Callable[[web.Request], Awaitable[web.Response]]:
+    async def handler(request: web.Request) -> web.Response:
+        try:
+            return await func(request)
+        except Exception as ex:
+            return web.json_response(
+                {"status": "failed", "reason": str(ex)}, status=400,
+                dumps=lambda data: json.dumps(data, ensure_ascii=False, indent=4)
+            )
+
+    return handler
+
+def build_response(data=None):
+    return web.json_response(
+                {"status": "success", "data": data} if data else {"status": "success"},
+                dumps=lambda data: json.dumps(data, ensure_ascii=False, indent=4)
+        )
--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,6 @@
 import json
 import asyncio
 
-from typing import Awaitable, Callable
-
 from aiohttp import web
 
 from db_engine import (
@@ -14,6 +12,8 @@ from db_engine import (
     update_in_securities,
     fetch_filtered_securities
 )
+
+from route_utils import handle_incorrect_name, handle_error, build_response
 
 
 router = web.RouteTableDef()
@@ -43,46 +43,6 @@ async def history_create(request: web.Request) -> web.Response:
     return web.json_response({"status": "success"})
 
 
-#-------------------DECORATORS-----------------
-
-ALLOWED_NAME_SYMBOLS = [chr(i) for i in range(ord('а'), ord('я') + 1)] +\
-    [chr(i) for i in range(ord('А'), ord('Я') + 1)] +\
-    [chr(i) for i in range(ord('0'), ord('9') + 1)] + [' ']
-
-def handle_incorrect_name(
-        func: Callable[[web.Request], Awaitable[web.Response]]
-    ) -> Callable[[web.Request], Awaitable[web.Response]]:
-    async def handler(request: web.Request) -> web.Response:
-        json_from_request = await request.json()
-
-        if not "name" in json_from_request:
-            return await func(request)
-        elif all(map(
-                lambda symbol: symbol in ALLOWED_NAME_SYMBOLS,
-                json_from_request["name"]
-        )):
-            return await func(request)
-        else:
-            raise ValueError("Incorrect symbol in 'name'")
-
-    return handler
-
-def handle_json_error(
-        func: Callable[[web.Request], Awaitable[web.Response]]
-    ) -> Callable[[web.Request], Awaitable[web.Response]]:
-    async def handler(request: web.Request) -> web.Response:
-        try:
-            return await func(request)
-        except asyncio.CancelledError:
-            raise
-        except Exception as ex:
-            return web.json_response(
-                {"status": "failed", "reason": str(ex)}, status=400
-            )
-
-    return handler
-
-
 #-------------------SECURITIES-----------------
 
 @router.get("/securities/all")
@@ -97,27 +57,24 @@ async def securities_all(request: web.Request) -> web.Response:
 async def securities_filtered(request: web.Request) -> web.Response:
     json_from_request = await request.json()
     res = await fetch_filtered_securities(json_from_request)
-    return web.json_response(
-                res,
-                dumps=lambda data: json.dumps(data, ensure_ascii=False, indent=4)
-        )
+    return build_response(res)
 
 @router.post("/securities/create")
 @handle_incorrect_name
 async def securities_create(request: web.Request) -> web.Response:
     json_from_request = await request.json()
     await insert_into_securities(json_from_request)
-    return web.json_response({"status": "success"})
+    return build_response()
 
 @router.post("/securities/update")
 @handle_incorrect_name
 async def securities_update(request: web.Request) -> web.Response:
     json_from_request = await request.json()
     await update_in_securities(json_from_request)
-    return web.json_response({"status": "success"})
+    return build_response()
 
 @router.post("/securities/delete")
 async def securities_delete(request: web.Request) -> web.Response:
     json_from_request = await request.json()
     await delete_from_securities(json_from_request["secid"])
-    return web.json_response({"status": "success"})
+    return build_response()
